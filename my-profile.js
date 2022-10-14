914f98332d285f425f27bdb355ae8fe5decde9ba
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-13 23:27:32 -0700

    add h1 ul

--- my-profile.js
+++ my-profile.js
@@ -1,1 +1,15 @@
-console.log('hii');
+const h1 = document.createElement('h1');
+h1.id = 'h1';
+h1.innerText = 'bt';
+
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+li1.textContent = 'b';
+const li2 = document.createElement('li');
+li2.textContent = 't';
+
+ul.appendChild(li1);
+ul.appendChild(li2);
+
+document.body.appendChild(h1);
+document.body.appendChild(ul);
