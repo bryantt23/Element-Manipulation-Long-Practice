9799f8d48e7f2edf685ec501f10df038db92b100
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-15 00:59:26 -0700

    add classes & styles

--- my-profile.js
+++ my-profile.js
@@ -8,6 +8,7 @@ function addLiElements(arr) {
   for (const elem of arr) {
     const li = document.createElement('li');
     li.textContent = elem;
+    li.setAttribute('class', 'detail');
     ul.appendChild(li);
   }
 }
@@ -16,3 +17,6 @@ addLiElements(['hi', 'how', 'goes?']);
 
 document.body.appendChild(h1);
 document.body.appendChild(ul);
+
+h1.setAttribute('class', 'name');
+ul.setAttribute('class', 'my-details');
